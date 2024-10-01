722b6e723848b921947cad70f03563fbe91b659c
Oskar Podrucki <118886547+oskarpodrucki@users.noreply.github.com>
2024-10-01 10:30:42 +0200

    ...

---
+++ src/app/page.js
@@ -0,0 +1,26 @@
+import NameList from "@/components/NameList"
+
+export default function Home() {
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen gap-5" >
+    <NameList name="oskarpodrucki" pay="no" ok/>
+    <NameList name="oskarpodrucki" ok/>
+    <NameList name="oskarpodrucki"/>
+    <NameList name="oskarpodrucki" pay="no"/>
+    <NameList name="oskarpodrucki" pay="no"/>
+    <NameList name="oskarpodrucki"/>
+    <NameList name="oskarpodrucki" ok/>
+    <NameList name="oskarpodrucki" ok/>
+    <NameList name="oskarpodrucki" ok/>
+    <NameList name="oskarpodrucki"/>
+    <NameList name="oskarpodrucki" pay="no"/>
+    <NameList name="oskarpodrucki"/>
+    <NameList name="oskarpodrucki" ok/>
+    <NameList name="oskarpodrucki" ok/>
+    <NameList name="oskarpodrucki" pay="no"/>
+    <NameList name="oskarpodrucki" pay="no"/>
+    </div>
+  )
+
+}
